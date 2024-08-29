450f8379b52a9b4fa25616a1dc32a7ec15cde6b3
ForkΨKILLET <fork_killet@outlook.com>
2024-08-30 02:03:39 +0800

    feat: basic reactive service

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,45 @@
-import { Context, Schema } from 'koishi'
+import { Context, Create, Keys, Query, Service, Tables } from 'koishi'
+import { reactive } from '@vue/reactivity'
+import { watch } from '@vue-reactivity/watch'
 
-export const name = 'w-reactive'
+declare module 'koishi' {
+    interface Context {
+        reactive: ReactiveService
+    }
+}
 
-export interface Config {}
+export type Reactive<T> = {
+    reactive: T
+    dispose: () => void
+    patch: (fn: (raw: T) => any) => Promise<void>
+}
 
-export const Config: Schema<Config> = Schema.object({})
+export default class ReactiveService<C extends Context = Context> extends Service {
+    static readonly inject = [ 'database' ]
 
-export function apply(ctx: Context) {
-  // write your plugin here
+    constructor(ctx: C) {
+        super(ctx, 'reactive')
+    }
+
+    async create<K extends Keys<Tables>>(
+        table: K,
+        query: Query<Tables[K]>,
+        defaultValue: Create<Tables[K], Tables>
+    ): Promise<Reactive<Tables[K]>> {
+        const raw = await this.ctx.database.get(table, query)[0] ?? (
+            this.ctx.database.create(table, defaultValue),
+            defaultValue
+        )
+        const proxy = reactive(raw)
+        const update = () => this.ctx.database.set(table, query, raw)
+        const unwatch = watch(proxy, update)
+        return {
+            reactive: proxy,
+            dispose: unwatch,
+            patch: async (fn) => {
+                await fn(raw)
+                await update()
+            }
+        }
+    }
 }
